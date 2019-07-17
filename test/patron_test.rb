caa7d765514bdfbe77a5ebd1eacc86ea324c173a
Alec Wells <alec.wells47@gmail.com>
2019-07-17 14:16:51 -0600

    Initial commit

---
+++ test/patron_test.rb
@@ -0,0 +1,12 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/exhibit'
+require './lib/patron'
+
+class PatronTrackerTest < Minitest::Test
+
+
+
+end
